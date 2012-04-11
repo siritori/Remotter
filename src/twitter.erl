5dd3cca4d6842d9b7d0cce2cc8e409b15edb67b5
Eric Sartre <s1111355@coins.tsukuba.ac.jp>
2012-04-12 03:10:24 +0900

    direct_msgモジュールじゃなくてtwitterモジュールって名前に変更。

---
+++ src/twitter.erl
@@ -0,0 +1,15 @@
+-module(twitter).
+-compile(export_all).
+-define(CONSUMER_KEY, "tdPkd4z4lvANKT22xqIzng").
+-define(CONSUMER_SECRET, "oZjO8LxzGSq4jvWcgRNOFzlJsiMLCiZOVMWJdIKPCsk").
+-define(ACCESS_TOKEN, "547781917-jNire46819xGXASVB4FBvlVDvIJWIz6XmvNFs3U8").
+-define(TOKEN_SECRET, "vWvZDu80doIoA8yIOeKkQ98t5fMErYgFMvimE7YeMU").
+
+send_dm(To, Msg) ->
+   application:start(inets),
+   ssl:start(),
+   Consumer = {?CONSUMER_KEY, ?CONSUMER_SECRET, hmac_sha1},
+   URL = "https://api.twitter.com/1/direct_messages/new.json",
+   Query = [{"screen_name", To}, {"text", Msg}],
+   oauth:post(URL, Query, Consumer, ?ACCESS_TOKEN, ?TOKEN_SECRET, []).
+
